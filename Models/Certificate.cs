168819620f426d28d5041f35063ed21654fd1530
webuilddeveloper <webuild.developer@gmail.com>
2025-07-21 16:37:28 +0700

    Initial Commit

---
+++ Models/Certificate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace cms_api.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Certificate : Identity
+    {
+
+        public Certificate()
+        {
+            imageUrl = "";
+            imageUrlCreateBy = "";
+            certified = "";
+        }
+
+        public string imageUrl { get; set; }
+        public string imageUrlCreateBy { get; set; }
+        public string certified { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace cms_api.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Product : Identity
+    {
+
+        public Product()
+        {
+            imageUrl = "";
+            imageUrlCreateBy = "";
+        }
+
+        public string imageUrl { get; set; }
+        public string imageUrlCreateBy { get; set; }
+    }
+}
