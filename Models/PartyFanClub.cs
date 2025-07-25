1db6f6bb926c316e1206e751b9a02319f2867364
webuilddeveloper <106293550+webuilddeveloper@users.noreply.github.com>
2025-07-25 13:57:00 +0700

    update party fan club

---
+++ Models/PartyFanClub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace cms_api.Models
+{
+    public class PartyFanClub : Identity
+    {
+        public PartyFanClub()
+        {
+            idcard = "";
+            prefixName = "";
+            firstName = "";
+            lastName = "";
+            birthDay = "";
+            phone = "";
+            email = "";
+      
+
+        }
+
+        public string idcard { get; set; }
+        public string prefixName { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string birthDay { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+ 
+
+    }
+}
