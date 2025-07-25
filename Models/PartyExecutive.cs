de7ef9d143ec5eaf4935992548f413ff31f634fb
webuilddeveloper <106293550+webuilddeveloper@users.noreply.github.com>
2025-07-25 16:16:04 +0700

    update partExecutive

---
+++ Models/PartyExecutive.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cms_api.Models
+{
+    public class PartyExecutive : Identity
+    {
+        public PartyExecutive()
+        {
+            imageUrl = "";
+        
+            gallery = new List<Gallery>();
+        }
+
+        public string imageUrl { get; set; }
+    
+
+        public List<Gallery> gallery { get; set; }
+    }
+}
