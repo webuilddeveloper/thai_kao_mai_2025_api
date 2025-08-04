f9df716b476b9353fb46d498827708e1001b23a8
webuilddeveloper <106293550+webuilddeveloper@users.noreply.github.com>
2025-08-04 10:49:22 +0700

    update party fan club

--- Models/PartyFanClub.cs
+++ Models/PartyFanClub.cs
@@ -23,7 +23,11 @@ namespace cms_api.Models
         public string birthDay { get; set; }
         public string phone { get; set; }
         public string email { get; set; }
- 
+
+        public bool volunteerWorkArea { get; set; }
+        public bool thinkTank { get; set; }
+        public bool trainingDemocrats { get; set; }
+        public bool applyPartyMembership { get; set; }
 
     }
 }
